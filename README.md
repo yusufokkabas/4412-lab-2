5a26caccf86d32ea2fb0b4c6624b660b8d61aa3f
Yusuf Orhan Okkabas <43817874+yusufokkabas@users.noreply.github.com>
2023-10-13 08:58:20 +0000

    first step done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 4412-lab-2
+### 4412-lab-2
+
+# 1. Using the image Rose1024.tif obtain the subsampled versions of the image as given given in the presentation, i.e. obtain the images of 512x512, 256x256, 128x128, 64x64 and 32x32 rose images.
+# 2. Use Ctskull-256.tif image to obtain different quantization levels of the image starting from 8 bits (original skull image), 7 bits, 6 bits, 5bits, 4 bits, 3 bits, 2 bits, and single bit images.
+
---
+++ practice.m
@@ -0,0 +1,42 @@
+i = imread('Rose1024.tif');
+imshow(i)
+
+figure
+imagesc(i)
+colormap gray
+i512 = i(1:2:end,1:2:end);
+
+figure
+imshow(i512),title('i 512x512')
+
+figure
+imagesc(i512), title('i scaled 512x512')
+colormap gray
+
+i256 = i(1:4:end,1:4:end);
+figure
+imshow(i256), title('i scaled to 256x256')
+
+clear;
+clc;
+close all;
+
+i = imread('Rose1024.tif');
+i512 = i(1:2:end,1:2:end)
+i256 = i(1:4:end,1:4:end)
+i128 = i(1:8:end,1:8:end)
+i64 = i(1:16:end,1:16:end)
+i32 = i(1:32:end,1:32:end)
+subplot(2,3,1);
+imshow(i); title('Original');
+subplot(2,3,4)
+imshow(i512); title('512');
+subplot(2,3,2)
+imshow(i256); title('256');
+subplot(2,3,5)
+imshow(i128); title('128');
+subplot(2,3,3)
+imshow(i64); title('64');
+subplot(2,3,6)
+imshow(i32); title('32');
+
